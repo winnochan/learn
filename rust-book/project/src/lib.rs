43325b3127decbb5900563ec971457d08ba66bff
winnochan <winnochan@icloud.com>
2021-05-10 11:02:35 +0800

    learn project

--- rust-book/project/src/lib.rs
+++ rust-book/project/src/lib.rs
@@ -1,4 +1,25 @@
 pub mod house {
+    #[derive(Debug)]
+    pub struct BreakFirst {
+        pub toast: String,
+        fruit: String,
+    }
+
+    impl BreakFirst {
+        pub fn summer(toast: &str, fruit: &str) -> BreakFirst {
+            BreakFirst {
+                toast: String::from(toast),
+                fruit: String::from(fruit),
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub mod hosting {
         pub fn add_to_waitlist() {
             println!("add_to_waitlist");
@@ -6,6 +27,7 @@ pub mod house {
 
         pub fn seat_at_table() {
             println!("seat_at_table");
+            super::serving::serve_order();
         }
     }
 
--- rust-book/project/src/main.rs
+++ rust-book/project/src/main.rs
@@ -1,6 +1,18 @@
-use project::house::hosting::add_to_waitlist;
+use project::house::{
+    hosting::{add_to_waitlist, seat_at_table},
+    Appetizer, BreakFirst,
+};
 
 fn main() {
-    // println!("Hello, world!");
     add_to_waitlist();
+    seat_at_table();
+    project::house::serving::serve_order();
+
+    let mut bf = BreakFirst::summer("a", "b");
+    println!("{:?}", bf);
+    bf.toast = String::from("c");
+    // bf.fruit = String::from("d");
+    // println!("{:?},{},{}", bf, bf.toast, bf.fruit);
+
+    println!("{:?},{:?}", Appetizer::Soup, Appetizer::Salad);
 }
