066a659eacbea0d18d7e87a2315326e8ee2fe73e
winnochan <winnochan@icloud.com>
2021-05-14 19:21:56 +0800

    learn smart pointer

--- rust/book/pointer/src/main.rs
+++ rust/book/pointer/src/main.rs
@@ -1,3 +1,15 @@
+use List::{Cons, Null};
+
 fn main() {
-    println!("Hello, world!");
+    let b = Box::new(5);
+    println!("b={}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Null))))));
+    println!("list={:?}", list);
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Null,
 }
