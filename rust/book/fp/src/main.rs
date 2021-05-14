d86a376024d197aead0d912a9adbfe2f032687e3
winnochan <winnochan@icloud.com>
2021-05-14 16:30:28 +0800

    learn functional programming

---
+++ rust/book/fp/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let v = Vector::<i32, 4> { list: [1, 2, 3, 4] };
+    println!("{:?}", v);
+
+    let v = vec![1, 2, 3];
+    println!("{:?}", v.iter().map(|x| x + 1).collect::<Vec<_>>());
+}
+
+#[derive(Debug)]
+struct Vector<T, const L: usize> {
+    list: [T; L],
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
