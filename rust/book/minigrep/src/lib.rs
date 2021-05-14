3e20d074a4e32a139f69ac7dddaec30e0dbad436
winnochan <winnochan@icloud.com>
2021-05-14 17:36:09 +0800

    optimize io project

--- rust/book/minigrep/src/lib.rs
+++ rust/book/minigrep/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error, fs};
+use std::{env, error, fs};
 
 #[derive(Debug)]
 pub struct Config {
@@ -7,14 +7,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        })
+    // pub fn new(args: &[String]) -> Result<Config, &str> {
+    //     if args.len() < 3 {
+    //         return Err("not enough arguments");
+    //     }
+    //     Ok(Config {
+    //         query: args[1].clone(),
+    //         filename: args[2].clone(),
+    //     })
+    // }
+
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        Ok(Config { query, filename })
     }
 }
 
--- rust/book/minigrep/src/main.rs
+++ rust/book/minigrep/src/main.rs
@@ -2,8 +2,8 @@ use minigrep::{run, Config};
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
