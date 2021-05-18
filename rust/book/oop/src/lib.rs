b03d6ae8e7cf7fd11f2d44979c9514afd01ea9c3
winnochan <winnochan@icloud.com>
2021-05-18 16:53:36 +0800

    learn oop

--- rust/book/oop/src/lib.rs
+++ rust/book/oop/src/lib.rs
@@ -11,4 +11,72 @@ impl AveragedCollection {
             average: 0f64,
         }
     }
+
+    pub fn add(&mut self, val: i32) {
+        self.list.push(val);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        if let Some(_) = result {
+            self.update_average();
+        }
+        result
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for c in self.components.iter() {
+            c.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!(
+            "label={},width={},height={}",
+            self.label, self.width, self.height
+        );
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!(
+            "options={:?},width={},height={}",
+            self.options, self.width, self.height
+        );
+    }
 }
--- rust/book/oop/src/main.rs
+++ rust/book/oop/src/main.rs
@@ -1,6 +1,32 @@
 mod lib;
+use crate::lib::{AveragedCollection, Button, Screen, SelectBox};
 
 fn main() {
-    let a = lib::AveragedCollection::new();
+    let mut a = AveragedCollection::new();
     println!("a = {:?}", a);
+
+    a.add(1);
+    a.add(2);
+    a.add(2);
+    println!("a = {:?}", a);
+    a.remove();
+    println!("a = {:?}", a);
+
+    println!("average = {}", a.average());
+
+    let screen = Screen {
+        components: vec![
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("Ok"),
+            }),
+            Box::new(SelectBox {
+                width: 100,
+                height: 200,
+                options: vec![String::from("Yes"), String::from("No")],
+            }),
+        ],
+    };
+    screen.run();
 }
