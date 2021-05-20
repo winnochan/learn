6ce1915b9b18bb84a4c3d63402709b32d9f45dc8
winnochan <winnochan@icloud.com>
2021-05-20 14:08:21 +0800

    learn oop

--- rust/book/oop/src/lib.rs
+++ rust/book/oop/src/lib.rs
@@ -99,12 +99,59 @@ impl Post {
     }
 
     pub fn request_review(&mut self) {
-        self.content.clear();
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review());
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
     }
 }
 
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
--- rust/book/oop/src/main.rs
+++ rust/book/oop/src/main.rs
@@ -30,6 +30,16 @@ fn main() {
     };
     screen.run();
 
-    // let mut post = Post::new();
+    let mut post = Post::new();
     // println!("post={:?}", post);
+
+    post.add_text("I ate a salad for lunch today");
+    // assert_eq!("", post.content());
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    post.request_review();
+    // assert_eq!("", post.content());
+
+    post.approve();
+    // assert_eq!("I ate a salad for lunch today", post.content());
 }
