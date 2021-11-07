dec4cdaabac0f84b1449babf1aa3f8a8618e5f8d
WinnoChan <winnochan@icloud.com>
2021-11-07 23:17:21 +0800

    add task1 solution

--- coursera/data-structures-algorithms/1.algorithmic-toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ coursera/data-structures-algorithms/1.algorithmic-toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,9 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    arr = [0, 1]
+    for i in range(2, n + 1):
+        arr.append(arr[i- 2] + arr[i -1 ])
+    return arr[n]
 
 n = int(input())
 print(calc_fib(n))
