548acfa6bc85f8d2a37cdf6341a7e06d2bfbf20d
WinnoChan <winnochan@icloud.com>
2021-11-08 10:58:42 +0800

    add solution for fib

--- coursera/data-structures-algorithms/1.algorithmic-toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
+++ coursera/data-structures-algorithms/1.algorithmic-toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
@@ -1,21 +1,25 @@
 // by Alexander Nikolskiy
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false,
 });
 
-process.stdin.setEncoding('utf8');
-rl.on('line', readLine);
+process.stdin.setEncoding("utf8");
+rl.on("line", readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    // write your code here
+  const arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
 }
 
 module.exports = fib;
