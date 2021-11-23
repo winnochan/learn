14d04732716edc7c4d9991f535aa0860a9beffa8
WinnoChan <winnochan@icloud.com>
2021-11-24 02:56:51 +0800

    add solution

--- coursera/data-structures-algorithms/1.algorithmic-toolbox/week4_divide_and_conquer/4_improving_quicksort.js
+++ coursera/data-structures-algorithms/1.algorithmic-toolbox/week4_divide_and_conquer/4_improving_quicksort.js
@@ -12,14 +12,34 @@ rl.once("line", () => {
       .split(" ")
       .map((s) => +s.trim());
 
-    console.log(quickSort(arr));
+    console.log(arr.slice(0).sort((a, b) => a - b));
+    console.log(quickSort3Way(shuffle(arr)));
     process.exit();
   });
 });
 
-function quickSort(arr = []) {
-  arr.sort((a, b) => a - b);
+function shuffle(arr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    const j = Math.floor(arr.length * Math.random());
+    if (i != j) {
+      const t = arr[i];
+      arr[i] = arr[j];
+      arr[j] = t;
+    }
+  }
   return arr;
 }
 
-module.exports = quickSort;
+function partition(arr, l, r) {
+  //
+}
+
+function quickSort3Way(arr = [], l = 0, r = null) {
+  if (r === null) r = arr.length - 1;
+  const [m1, m2] = partition(arr, l, r);
+  quickSort3Way(arr, l, m1 - 1);
+  quickSort3Way(arr, m2 + 1, r);
+  return arr;
+}
+
+module.exports = quickSort3Way;
