bfbf5acde80e23ec6c43354811b9b1a2b7856c11
WinnoChan <winnochan@icloud.com>
2021-12-05 22:43:38 +0800

    add solution

--- coursera/data-structures-algorithms/1.algorithmic-toolbox/week4_divide_and_conquer/4_improving_quicksort.js
+++ coursera/data-structures-algorithms/1.algorithmic-toolbox/week4_divide_and_conquer/4_improving_quicksort.js
@@ -4,7 +4,7 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-console.log(quickSort3Way([2, 4, 2, 3, 9, 7, 2, 5]));
+// console.log(quickSort3Way([2, 4, 2, 3, 9, 7, 2, 5]));
 
 process.stdin.setEncoding("utf8");
 rl.once("line", () => {
@@ -15,7 +15,7 @@ rl.once("line", () => {
       .map((s) => +s.trim());
 
     // console.log(arr.slice(0).sort((a, b) => a - b));
-    // console.log(quickSort3Way(arr));
+    console.log(quickSort3Way(arr));
     process.exit();
   });
 });
@@ -32,82 +32,40 @@ function shuffle(arr = []) {
   return arr;
 }
 
+function swap(arr = [], j, k) {
+  [arr[j], arr[k]] = [arr[k], arr[j]];
+}
+
 // 2,3,4,2,5,9,7,2
 // 2 4 3 2 5 9 7 2
 // 2 2 3 2 5 9 7 4
 function partition(arr = [], left, right) {
-  if (right - left <= 1) return arr;
-  if (right - left == 2) {
-    if (arr[left] > arr[right]) {
-      const temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
+  const val = arr[left];
+  let mid = left;
+  while (mid <= right) {
+    if (arr[mid] === val) {
+      mid++;
+    } else if (arr[mid] > val) {
+      swap(arr, mid, right--);
+    } else {
+      swap(arr, mid, left++);
     }
-    return arr;
   }
-  const val = arr[left + 1];
-  let mid = left + 1;
-  let temp;
-  while (mid < right) {
-    while (arr[right] > val) right--;
-    if (arr[right] == val && mid + 1 < right) {
-      mid += 1;
-      temp = arr[mid];
-      arr[mid] = arr[right];
-      arr[right] = temp;
-    }
-
-    while (arr[left] < val) left++;
-    if (arr[left] == val && left <= mid) {
-      mid += 1;
-      temp = arr[mid];
-      arr[mid] = arr[left];
-      arr[left] = temp;
-    }
-
-    if (left < right && arr[left] > arr[right]) {
-      temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
-    }
-
-    console.log(JSON.stringify(arr), val, left, right);
-    // while (mid < right && arr[right] >= val) {
-    //   if (arr[right] == val) {
-    //     mid += 1;
-    //     arr[right] = arr[mid];
-    //     arr[mid] = val;
-    //   } else {
-
-    //   }
-    // }
-
-    // while (mid < right && arr[left] <= val) {
-    //   if (arr[left] == val) {
-    //     mid += 1;
-    //     arr[left] = arr[mid];
-    //     arr[mid] = val;
-    //   } else {
-
-    //   }
-    // }
-  }
-  // console.log(left, right);
-  return [left - 1, right];
+  return [left, right];
 }
 
 function quickSort3Way(arr = [], l = 0, r = null) {
   if (r === null) {
     r = arr.length - 1;
-    // shuffle(arr);
+    shuffle(arr);
     // console.log("shuffle", arr);
   }
   const [m1, m2] = partition(arr, l, r);
-  if (l < m1) {
-    quickSort3Way(arr, l, m1);
+  if (l < m1 - 1) {
+    quickSort3Way(arr, l, m1 - 1);
   }
-  if (m2 < r) {
-    quickSort3Way(arr, m2, r);
+  if (m2 + 1 < r) {
+    quickSort3Way(arr, m2 + 1, r);
   }
   return arr;
 }
