bbe6007ea4a100d040fe129d6e86871d873d80e2
WinnoChan <winnochan@163.com>
2024-05-01 20:25:29 +0800

    lc ac 97 mid

--- oj/leetcode/cpp/00097.interleaving-string/solution.cpp
+++ oj/leetcode/cpp/00097.interleaving-string/solution.cpp
@@ -29,23 +29,30 @@ public:
                 return false;
             }
         }
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
-        }
+        // vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        // dp[0][0] = true;
+        // for (int i = 1; i <= m; i++) {
+        //     dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
+        // }
+        // for (int i = 1; i <= n; i++) {
+        //     dp[0][i] = dp[0][i - 1] && s2[i - 1] == s3[i - 1];
+        // }
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
         for (int i = 1; i <= n; i++) {
-            dp[0][i] = dp[0][i - 1] && s2[i - 1] == s3[i - 1];
+            dp[i] = dp[i - 1] && s2[i - 1] == s3[i - 1];
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
-                           (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                // dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                //            (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) ||
+                        (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
             }
         }
         // printMat(dp);
 
-        return dp[m][n];
+        return dp[n];
     }
 
     void printMat(vector<vector<bool>>& mat) {
